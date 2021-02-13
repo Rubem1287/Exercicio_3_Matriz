833743fdbdd9a4b0a5fe5b3f49d0bece5f55d294
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-13 18:59:25 -0300

    Add files via upload

---
+++ Exercício 3 M/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercício_3_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a;
+
+            int n = int.Parse(Console.ReadLine());
+
+            a = new int[n, n];
+
+            int maior = 0;
+
+            for(int l=0;l<n;l++)
+            {
+                string[] vetor = Console.ReadLine().Split(' ');
+                for(int c= 0;c<n;c++ )
+                {
+                    a[l, c] = int.Parse(vetor[c]);
+                }
+            }
+
+            Console.WriteLine();
+            for (int l = 0; l < n; l++)
+            {
+                
+                for (int c = 0; c < n; c++)
+                {
+                    if (maior == 0)
+                    {
+                        maior = a[l, c];
+                    }
+                    else if (maior < a[l,c])
+                    {
+                        maior = a[l, c];
+                    }
+                }
+
+                Console.WriteLine(maior);
+                maior = 0;
+
+            }
+        }
+    }
+}
